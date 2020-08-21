1919889b4b20b466d8d5bd903b275844449ea69f
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-21 05:35:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
+  
+  
   # Use `each_with_index` to enumerate over the provided array
-  #
   # Print a numbered list of each element
 end
